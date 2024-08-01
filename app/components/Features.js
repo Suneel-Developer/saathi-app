21ae14a6e652250f11708acad92a5280518cf567
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-08-01 21:03:01 +0500

    update

--- app/components/Features.js
+++ app/components/Features.js
@@ -11,7 +11,7 @@ const Features = () => {
                 </div>
 
                 <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto'>
-                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-auto h-[345px] lg:h-[470px] mx-auto'>
+                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-full h-[345px] lg:h-[470px] mx-auto'>
                         <div className='features1'></div>
                         <div className='flex flex-col absolute top-0 bottom-0 w-full h-full left-0 z-30 items-center justify-center bg-[#FAF8FF] rounded-[45px] px-4 md:px-10 py-4 md:py-10'>
                             <img src="/assets/features1.webp" alt="3D Flight Ticket Model With Airplane Icon" className='h-[189px] md:h-[266px]' />
@@ -22,7 +22,7 @@ const Features = () => {
                         </div>
                     </div>
 
-                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-auto h-[345px] lg:h-[470px] mx-auto'>
+                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-full h-[345px] lg:h-[470px] mx-auto'>
                         <div className='features2'></div>
                         <div className='flex flex-col absolute top-0 bottom-0 w-full h-full left-0 z-30 items-center justify-center bg-[#FAF8FF] rounded-[45px] px-4 md:px-10 py-4 md:py-10'>
                             <img src="/assets/features2.webp" alt="monthly-bills" className='h-[189px] md:h-[266px]' />
@@ -33,7 +33,7 @@ const Features = () => {
                         </div>
                     </div>
 
-                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-auto h-[345px] lg:h-[470px] mx-auto'>
+                    <div className='relative flex items-end justify-center w-[310px] xxs:w-[345px] lg:w-full h-[345px] lg:h-[470px] mx-auto'>
                         <div className='features3'></div>
                         <div className='flex flex-col absolute top-0 bottom-0 w-full h-full left-0 z-30 items-center justify-center bg-[#FAF8FF] rounded-[45px] px-4 md:px-10 py-4 md:py-10'>
                             <img src="/assets/features3.webp" alt="3D-checklist-model-with-checked-boxes" className='h-[189px] md:h-[266px]' />
