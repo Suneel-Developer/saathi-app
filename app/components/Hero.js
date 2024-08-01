1abf1ffdb311310dbd7454c560093015515a60b2
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-08-01 21:12:46 +0500

    updates

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -6,14 +6,14 @@ import Header from './Header';
 const Hero = () => {
     const [isAndroid, setIsAndroid] = useState(false);
     const [isIOS, setIsIOS] = useState(false);
-  
+
     useEffect(() => {
-      if (typeof window !== 'undefined') {
-        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
-        setIsAndroid(/Android/i.test(userAgent));
-        setIsIOS(/iPhone|iPad|iPod/i.test(userAgent));
-      }
+        if (typeof window !== 'undefined') {
+            const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+            setIsAndroid(/Android/i.test(userAgent));
+            setIsIOS(/iPhone|iPad|iPod/i.test(userAgent));
+        }
     }, []);
     return (
         <section className="h-screen w-full relative bg-[url('/assets/mobile-img.jfif')] md:bg-none bg-cover bg-center bg-no-repeat px-4 md:px-0 flex items-center justify-center">
@@ -31,19 +31,20 @@ const Hero = () => {
                     <img src="/assets/qr.svg" alt="qr" className='mt-3 hidden md:flex' />
 
                     <p className='text-center text-black md:text-white font-semibold md:font-bold leading-[16.37px] md:leading-[32.74px] text-xs md:text-2xl mt-10 md:mt-12'>The app experience is unbeatable!</p>
-                    <button className='w-[200px] md:w-[239px] h-[50px] md:h-[60px] bg-white rounded-[50px] text-center text-xl md:text-2xl text-[#0A0A0A] font-semibold leading-[27.28px] md:leading-[32.74px] mt-7 md:mt-8 flex items-center justify-center gap-1'>
-                        <img src="/assets/apple.svg" alt="apple" className='block md:hidden h-7 w-[23px]' />
+                    <button className='w-[200px] md:w-[239px] h-[50px] hidden md:flex md:h-[60px] bg-white rounded-[50px] text-center text-xl md:text-2xl text-[#0A0A0A] font-semibold leading-[27.28px] md:leading-[32.74px] mt-7 md:mt-8 items-center justify-center gap-1'>
                         Download Now
                     </button>
 
                     {isAndroid && (
-                        <button>
-                            Android Button
+                        <button className='w-[200px] md:w-[239px] h-[50px] hidden md:flex md:h-[60px] bg-white rounded-[50px] text-center text-xl md:text-2xl text-[#0A0A0A] font-semibold leading-[27.28px] md:leading-[32.74px] mt-7 md:mt-8 items-center justify-center gap-1'>
+                            <img src="/assets/andriod.svg" alt="apple" className='h-7 w-[23px]' />
+                            Download Now
                         </button>
                     )}
                     {isIOS && (
-                        <button>
-                            Apple Button
+                        <button className='w-[200px] md:w-[239px] h-[50px] hidden md:flex md:h-[60px] bg-white rounded-[50px] text-center text-xl md:text-2xl text-[#0A0A0A] font-semibold leading-[27.28px] md:leading-[32.74px] mt-7 md:mt-8 items-center justify-center gap-1'>
+                            <img src="/assets/apple.svg" alt="apple" className='h-7 w-[23px]' />
+                            Download Now
                         </button>
                     )}
                 </div>
